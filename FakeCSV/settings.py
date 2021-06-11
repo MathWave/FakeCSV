73f6a344824ea82c4285a9b387866cf6857ed8b8
Egor Matveev <emmtvv@icloud.com>
2021-06-11 14:33:02 +0300

    removed whitenoise

--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,9 +134,6 @@ DEFAULT_LOGIN_PAGE = '/enter'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'Main', 'static'),
 # )
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
