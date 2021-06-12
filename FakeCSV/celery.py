00f667c77b1da1d2d8536f77686b8864cb7c7b35
Egor Matveev <emmtvv@icloud.com>
2021-06-12 20:20:53 +0300

    celery

--- FakeCSV/celery.py
+++ FakeCSV/celery.py
@@ -13,5 +13,7 @@ app = Celery('FakeCSV')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
